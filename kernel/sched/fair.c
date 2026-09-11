4aa5e7f46d5e0e5fe028dce3a7ce4a413e843fec
Samw662 <amadorsamir307@gmail.com>
2026-09-11 11:26:26 -0600

    sched: fair: remove set_skip_buddy, dead CFS legacy
    set_skip_buddy writes to cfs_rq->skip which is never read or cleared
    after the clear_buddies cleanup. Mainline 6.6 doesn't have skip buddy
    at all. Remove the function and its call in yield_task_fair.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8023,12 +8023,6 @@ static void set_next_buddy(struct sched_entity *se)
 	}
 }
 
-static void set_skip_buddy(struct sched_entity *se)
-{
-	for_each_sched_entity(se)
-		cfs_rq_of(se)->skip = se;
-}
-
 /*
  * Preempt the current task with a newly woken task if needed:
  */
@@ -8304,8 +8298,6 @@ static void yield_task_fair(struct rq *rq)
 		 */
 		rq_clock_skip_update(rq);
 	}
-
-	set_skip_buddy(se);
 }
 
 static bool yield_to_task_fair(struct rq *rq, struct task_struct *p, bool preempt)
